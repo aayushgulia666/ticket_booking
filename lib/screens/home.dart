6d9bedeca71d0bf86d71d255778ef9b9b668f97e
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-20 19:05:57 +0530

    Home page icon and text added.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Home screen");
+    return Scaffold(
+      backgroundColor: const Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Good Morning"),
+                        Text("Book Tickets"),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          //fit: BoxFit.cover,
+                          image: AssetImage("assets/images/app_icon.png")
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      )
+    );
   }
 }
