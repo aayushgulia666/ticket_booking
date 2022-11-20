20d67a83131dc7e5646a3517cf90213e48abf987
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-20 22:56:51 +0530

    Search bar created..

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -39,9 +40,23 @@ class HomeScreen extends StatelessWidget {
                           image: AssetImage("assets/images/app_icon.png")
                         )
                       ),
-                    )
+                    ),
                   ],
-                )
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children:  [
+                      const Icon(FluentIcons.search_16_regular, color: Color(0xFFBFC205),),
+                      Text("Search", style: Styles.headlineStyle4,)
+                    ],
+                  ),
+                ),
               ],
             ),
           )
