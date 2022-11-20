a1a42d1ff1e07491c33c1024ae4d3df90b46c12a
Aayush Gulia <aayushgulia07@outlook.com>
2022-11-21 00:51:37 +0530

    description row added.

--- lib/ticket_view.dart
+++ lib/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:ticket_booking/utils/app_layout.dart';
 import 'package:ticket_booking/utils/app_styles.dart';
 import 'package:ticket_booking/widgets/circle_container.dart';
@@ -20,6 +21,9 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16, right: 16),
         child: Column(
           children: [
+            /*
+            Shows the blue part of the ticket.
+             */
             Container(
               decoration: const BoxDecoration(
                 color: Color(0xFF526799),
@@ -56,9 +60,34 @@ class TicketView extends StatelessWidget {
                       )),
                       const CircleContainer(),
                       Expanded(child: Container()),
-                      Text("London", style: Styles.headlineStyle3.copyWith(color: Colors.white),),
+                      Text("LDN", style: Styles.headlineStyle3.copyWith(color: Colors.white),),
                     ],
-                  )
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text("New York", style: Styles.headlineStyle4.copyWith(color: Colors.white),),
+                      ),
+                      Text("8H 30M", style: Styles.headlineStyle4.copyWith(color: Colors.white),),
+                      SizedBox(
+                        width: 100,
+                        child: Text("London", textAlign: TextAlign.end, style: Styles.headlineStyle4.copyWith(color: Colors.white),),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            /*
+            Shows the orange part of the ticket.
+             */
+            Container(
+              child: Row(
+                children: [
+                  SizedBox()
                 ],
               ),
             )
